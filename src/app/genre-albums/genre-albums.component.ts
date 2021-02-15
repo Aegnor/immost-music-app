6f0751812b10a56573d0e2d3cb3bdb84dbfa1a5b
Sbr <12alexman@gmail.com>
2021-02-15 23:02:53 +0200

    create album listing compenent

---
+++ src/app/genre-albums/genre-albums.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-genre-albums',
+  templateUrl: './genre-albums.component.html',
+  styleUrls: ['./genre-albums.component.scss']
+})
+export class GenreAlbumsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
