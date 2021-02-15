921052830e64eb2c2d1e26ea5810924c816b47d8
Sbr <12alexman@gmail.com>
2021-02-15 23:02:31 +0200

    create genre link component

---
+++ src/app/genre-links/genre-links.component.ts
@@ -0,0 +1,47 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-genre-link',
+  templateUrl: './genre-links.component.html',
+  styleUrls: ['./genre-links.component.scss']
+})
+export class GenreLinksComponent implements OnInit {
+  genresArray = [
+    {
+      name: 'Rock',
+      imgSrc: '../../assets/images/1.webp',
+      imgAlt: 'ralph tv cabin fever dreams'
+    },
+    {
+      name: 'Electro',
+      imgSrc: '../../assets/images/2.webp',
+      imgAlt: 'roman flugel garden party'
+    },
+    {
+      name: 'Hip-Hop',
+      imgSrc: '../../assets/images/3.webp',
+      imgAlt: 'two stone hands'
+    },
+    {
+      name: 'Pop',
+      imgSrc: '../../assets/images/4.webp',
+      imgAlt: 'Juan MacLean manthony'
+    },
+    {
+      name: 'R&B',
+      imgSrc: '../../assets/images/5.webp',
+      imgAlt: 'Las Vegas'
+    },
+    {
+      name: 'Indie',
+      imgSrc: '../../assets/images/6.webp',
+      imgAlt: 'Two kids'
+    }
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+}
