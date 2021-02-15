bf9b036295d10a719bd068e410619f0e5aaca426
Sbr <12alexman@gmail.com>
2021-02-15 23:04:04 +0200

    add home route, add genre album listing route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GenreAlbumsComponent } from './genre-albums/genre-albums.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'genre-albums/:tag',
+    component: GenreAlbumsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
