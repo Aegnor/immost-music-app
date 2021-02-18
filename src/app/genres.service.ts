66a6c29f07eb67822f1b4852f37c702eb521ffcb
Sbr <12alexman@gmail.com>
2021-02-18 22:24:00 +0200

    add genre api service

---
+++ src/app/genres.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class GenresService {
+  constructor(private http: HttpClient) {
+  }
+
+  fetchAllByGenre(genre: string): Observable<any> {
+    return this.http.get(`http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=${genre}&api_key=22e5dcb7293a23da484afeacce80c247&format=json`);
+  }
+}
