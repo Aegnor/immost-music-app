463189f6db0ae6136045b49c9de58f08271dae9b
Sbr <12alexman@gmail.com>
2021-02-20 02:44:24 +0200

    add filter pipe

---
+++ src/app/pipes/filter.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(array: object[], search: string = ''): object[] {
+    if (!search.trim()) {
+      return array;
+    }
+
+    return array.filter(elem => {
+      // @ts-ignore
+      return elem.name.toLowerCase().includes(search.toLowerCase());
+    });
+  }
+
+}
