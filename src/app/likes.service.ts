2aa5c91eb8608d0c4269b63709bde9903d29c32e
Sbr <12alexman@gmail.com>
2021-02-20 02:44:10 +0200

    create like service

---
+++ src/app/likes.service.ts
@@ -0,0 +1,45 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+export class LikesService {
+  likesArray: {
+    name: string;
+  }[] = [];
+
+  constructor() {
+  }
+
+  getLikesLength(): number {
+    return this.likesArray.length;
+  }
+
+  getLikesArrayFromLocalStorage(): any {
+    const likes = localStorage.getItem('likes');
+    if (likes != null) {
+      return this.likesArray = JSON.parse(likes);
+    }
+  }
+
+  isLiked(name: string): boolean {
+    const candidate = this.likesArray.find(item => item.name === name);
+
+    return candidate ? true : false;
+  }
+
+  addToLikes(item: {
+    isLiked: boolean;
+    name: string
+  }): void {
+      this.likesArray.push(item);
+
+      localStorage.setItem('likes', JSON.stringify(this.likesArray));
+  }
+
+  removeFromLikes(name: string): void {
+    const candidateIdx = this.likesArray.findIndex(item => item.name === name);
+
+    this.likesArray.splice(candidateIdx, 1);
+
+    localStorage.setItem('likes', JSON.stringify(this.likesArray));
+  }
+}
